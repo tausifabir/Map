bac99ffcca58d55ba72033a5f822201b229e689d
tausifabir <tausifabir94@gmail.com>
2020-06-28 16:03:41 +0600

    firebase Store

--- app/src/main/java/com/example/map/MainActivity.java
+++ app/src/main/java/com/example/map/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.map;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.os.Bundle;
 import android.os.Looper;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationAvailability;
 import com.google.android.gms.location.LocationCallback;
@@ -20,16 +26,28 @@ import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.location.LocationSettingsResponse;
 import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView latTV;
     private TextView lngTV;
+    private TextView firebaselatTV;
+    private TextView firebaselngTV;
 
     FusedLocationProviderClient fusedLocationProviderClient;
     private Location location;
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,9 +63,15 @@ public class MainActivity extends AppCompatActivity {
 
         latTV = findViewById(R.id.latTV);
         lngTV = findViewById(R.id.lngTV);
-
+        firebaselatTV = findViewById(R.id.firebaselatTV);
+        firebaselngTV = findViewById(R.id.firebaselngTV);
 
     }
+
+
+
+
+
 
 
     LocationCallback locationCallback = new LocationCallback() {
@@ -60,6 +84,43 @@ public class MainActivity extends AppCompatActivity {
 
                 latTV.setText("Latitude: "+location.getLatitude());
                 lngTV.setText("Longitude: "+location.getLongitude());
+
+
+                double lat =  location.getLatitude();
+                double lng =  location.getLongitude();
+
+
+                Map<String, Object> myLocations =  new HashMap<>();
+                myLocations.put("lat",lat);
+                myLocations.put("lng",lng);
+
+                db.collection("MyLocations").document("Current Locations").set(myLocations)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                latTV.setText(""+e);
+                            }
+                        });
+
+                db.collection("MyLocations").document("Current Locations").get()
+                        .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                            @Override
+                            public void onSuccess(DocumentSnapshot documentSnapshot) {
+
+
+
+
+                                firebaselatTV.setText(String.valueOf(documentSnapshot.getDouble("lat")));
+                                firebaselatTV.setText(String.valueOf(documentSnapshot.getDouble("lng")));
+
+                            }
+                        });
             }
         }
 
